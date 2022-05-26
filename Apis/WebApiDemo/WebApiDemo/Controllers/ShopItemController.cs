0c082c657beb6814a3b793f37c99c097d6512808
JauniusPinelis <JauniusPinelis@gmail.com>
2022-05-26 18:56:16 +0300

    Api

---
+++ Apis/WebApiDemo/WebApiDemo/Controllers/ShopItemController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApiDemo.Models;
+using WebApiDemo.Services;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShopItemController : ControllerBase
+    {
+        private readonly ShopItemService _shopItemService;
+
+        public ShopItemController(ShopItemService shopItemService)
+        {
+            _shopItemService = shopItemService;
+        }
+
+        [HttpGet]
+        public List<ShopItem> GetAll()
+        {
+            return _shopItemService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ShopItem GetAll(int id)
+        {
+            return _shopItemService.Get(id);
+        }
+
+        [HttpPost]
+        public void Create(ShopItem shopItem)
+        {
+            _shopItemService.Add(shopItem);
+        }
+
+        [HttpPut]
+        public void Update(ShopItem shopItem)
+        {
+            _shopItemService.Update(shopItem);
+        }
+
+        [HttpDelete("{id}")]
+        public void Remove(int id)
+        {
+            _shopItemService.Remove(id);
+        }
+    }
+}
---
+++ Apis/WebApiDemo/WebApiDemo/Services/ShopItemService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Services
+{
+    public class ShopItemService
+    {
+        private List<ShopItem> _shopItems = new List<ShopItem>()
+        {
+            new ShopItem()
+            {
+                Name = "Ice cream",
+                Id = 1
+            },
+            new ShopItem()
+            {
+                Name = "Candy",
+                Id = 2
+            }
+        };
+
+        public ShopItem Get(int id)
+        {
+            return _shopItems.First(s => s.Id == id);
+        }
+
+        public List<ShopItem> GetAll()
+        {
+            return _shopItems;
+        }
+
+        public void Add(ShopItem shopItem)
+        {
+            _shopItems.Add(shopItem);
+        }
+
+        public void Update(ShopItem shopItem)
+        {
+            var shopItemToUpdate = _shopItems.First(s => s.Id == shopItem.Id);
+
+            shopItemToUpdate.Name = shopItem.Name;
+        }
+
+        public void Remove(int id)
+        {
+            var shopItemToRemove = _shopItems.First(s => s.Id == id);
+
+            _shopItems.Remove(shopItemToRemove);
+        }
+
+    }
+}
