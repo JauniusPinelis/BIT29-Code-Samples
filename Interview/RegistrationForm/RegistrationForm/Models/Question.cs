1ae2ebbcf8edc6d0d1c3591de3fd861597bbff59
JauniusPinelis <JauniusPinelis@gmail.com>
2022-05-26 21:02:11 +0300

    Interview task:

---
+++ Interview/RegistrationForm/RegistrationForm/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace RegistrationForm.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public int QuestionValueAnswerId { get; set; }
+
+        public List<QuestionValue> QuestionValues { get; set; }
+    }
+}
---
+++ Interview/RegistrationForm/RegistrationForm/Models/QuestionValue.cs
@@ -0,0 +1,13 @@
+﻿namespace RegistrationForm.Models
+{
+    public class QuestionValue
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public Question Question { get; set; }
+    }
+}
